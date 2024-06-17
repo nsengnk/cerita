a0dbf227cb64dccb8e4fdb4859a95f9b6b350381
ZedFst <nzambabignoumba99@gmail.com>
2024-06-17 15:36:16 +0300

    Configuration du projet avec vue.js

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import NavigationBar from "./components/NavigationBar.vue";
+</script>
+
+<template>
+  <header><NavigationBar></NavigationBar></header>
+  <main></main>
+</template>
+
+<style scoped></style>
---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template><p>Here we are!</p></template>
+
+<style scoped></style>
